d87f5bf2301ff519ba322eade4e8b0b971be83d4
dennishkumar <240705.EV@rmkec.ac.in>
2026-06-05 23:01:54 +0530

    Add files via upload

---
+++ pangram checking(a-z).java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine().toLowerCase();
+        
+        boolean[] letters=new boolean[26];
+        int count=0;
+        
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch>='a'&&ch<='z'){
+                int index=ch-'a';
+                if(!letters[index]){
+                    letters[index]=true;
+                    count++;
+                }
+            }
+        }
+        
+        if(count==26){
+            System.out.println("Pangram");
+        }else{
+            System.out.println("Not Pangram");
+        }
+    }
+}
